df6bc4f8c8f3104911489552da43da89b6a1770c
Fataliti <hell.jnacho@gmail.com>
2021-07-16 17:19:47 -0400

    act. settings dotenv

--- turismo/turismo/settings.py
+++ turismo/turismo/settings.py
@@ -19,13 +19,13 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DJANGO_KEY = os.getenv('DJANGO_SECRET_KEY')
+DJANGO_SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = DJANGO_KEY
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
